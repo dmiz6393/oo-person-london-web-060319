3c5045bbf2f4f4cdfa54f138362390fedb83467d
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-05 13:56:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,21 @@
 class Person 
   
-  attr_accessor(:bank_account)
-  attr_reader(:name)
+  attr_accessor:bank_account, :happiness, :hygiene
+  attr_reader:name
   attr_writer 
   
   def initialize(name)
     @name = name
+    @bank_account= 25
+    @happiness= 8
+    @hygiene= 8 
   end
+  
+  def happiness(number) 
+    @happiness=number 
+    @happiness=10 
+    if happiness>10 
+      @happiness=0 if @happiness<0
+  end 
+  
 end 
